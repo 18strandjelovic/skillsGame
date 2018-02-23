f6a3b69ef72f39d4c4d59142fff8f295aa8d573f
Unknown <stefan@bigyugo.com>
2018-02-23 14:59:01 -0500

    trying to get the status script working

---
+++ Assets/scripts/characterStatus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterStatus : MonoBehaviour {
+    public float health = 10;
+    public int healthMax = 10;
+    public int stamina = 10;
+    public float heavyAttackValue = 4f;
+    public float runValue = 2f;
+
+    public Rigidbody2D rb;
+    public playerController pc;
+
+    public float addHealth(float boost) {
+        health += boost;
+
+        if (health > healthMax)
+        {
+            health = healthMax;
+        }
+        return (health);
+    }
+
+    //public 
+}
--- Assets/scripts/stickController.cs
+++ Assets/scripts/stickController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class chartercontroller : MonoBehaviour
+public class playerController : MonoBehaviour
 {
 
     float walkSpeed = 10;                       //walk speed
