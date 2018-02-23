51d773d682d54e49f3a1c22510ed3dee7516d309
Unknown <stefan@bigyugo.com>
2018-02-23 13:31:39 -0500

    this is the start of the game

---
+++ Assets/scripts/cameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+    public Transform player;
+    public float marginX = 3f;
+    public float marginY = 1f;
+    public float smoothX = 8f;
+    public float smoothY = 8f;
+    public Vector2 minXY;
+    public Vector2 maxXY;
+
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+    private bool CheckXMargin()
+    {
+        // Returns true if the distance between the camera and the player in the x axis is greater than the x margin.
+        return Mathf.Abs(transform.position.x - player.position.x) > marginX;
+    }
+
+    private bool CheckYMargin()
+    {
+        return Mathf.Abs(transform.position.y - player.transform.position.y) > marginY;
+    }
+
+    private void tracking() {
+        float targetX = transform.position.x;
+        float targetY = transform.position.y;
+
+        if (CheckXMargin()){
+            targetX = Mathf.Lerp(transform.position.x, player.transform.position.x, smoothX * Time.deltaTime);
+        }
+        if (CheckYMargin()){
+            targetY = Mathf.Lerp(transform.position.y, player.transform.position.y, smoothY * Time.deltaTime);
+        }
+
+        targetX = Mathf.Clamp(targetX, minXY.x, maxXY.x);
+        targetY = Mathf.Clamp(targetY, minXY.y, maxXY.y);
+
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        tracking();
+	}
+}
---
+++ Assets/scripts/chartercontroller.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chartercontroller : MonoBehaviour
+{
+
+    float walkSpeed = 10;                       //walk speed
+    public Rigidbody2D rb;                      //initialized the rigbody of the character
+    public float jumpSpeed = 8;                 //how fast character jumps
+    public Vector2 vectH = new Vector2(1, 0);   //direction for horizontal movement
+    public Vector2 vectV = new Vector2(0, 1);   //direction of vertical movemnet
+    public bool groundCheck = true;             //makes sure that the player is not moving vertically
+    public bool isJumping = false;              //check for jumping or not
+    public int facingRight = 0;                 //checks for moving in the positive direction, right is 1, left is 2, no direction is 0
+    public bool isMoving = false;               //check for if  the character is moving
+    float horizontal;                           //input for walking
+    float vertical;                             //input for jumping
+    float jumpH;                                //horizontal movement for jump
+
+    // Use this for initialization
+    void Start()
+    {
+        jumpH = walkSpeed / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if (rb.velocity.y != 0)
+        {    //check to see if the character is jumping
+            groundCheck = false;
+        }
+        else groundCheck = true;
+
+        if (rb.velocity.x != 0)
+        {    //check to see if the character is moving
+            isMoving = true;
+        }
+        else isMoving = false;
+
+        //Horizontal movement controlsd
+        if (isJumping == false)
+        {
+            if (horizontal > 0)
+            {
+                transform.Translate(vectH * walkSpeed * Time.deltaTime);     //Moves charactor right
+                facingRight = 1;                                         //shows that charactor is moving in the positive direction
+                isMoving = true;
+            }
+            else if (horizontal < 0)
+            {
+                transform.Translate(vectH * -1 * walkSpeed * Time.deltaTime);    //moves charactor left
+                facingRight = 2;                                            //shows that charactor is moving in the negative direction
+                isMoving = true;
+            }
+            else if (horizontal == 0)
+            {
+                facingRight = 0;
+                isMoving = false;
+            }
+        }
+
+        //Jump control
+        if (rb.velocity.y == 0)
+        {       //makes sure your on the ground
+            if (vertical > 0)
+            {          //input for jumping
+                isJumping = true;
+                rb.AddForce(vectV * jumpSpeed, ForceMode2D.Impulse);     //jumps
+
+                switch (facingRight)
+                {
+                    case 0:
+                        isMoving = false;
+                        break;
+                    case 1:
+                        if (isMoving == true)
+                        { //put this to encompas whole switch
+                            rb.AddForce(vectH * jumpH, ForceMode2D.Impulse);
+                        }
+                        break;
+                    case 2:
+                        if (isMoving == true)
+                        {
+                            rb.AddForce(vectH * -1 * jumpH, ForceMode2D.Impulse);
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+    }
+
+    public int FacingRight()
+    {
+        return facingRight;
+    }
+
+    public bool IsMoving()
+    {
+        return vectH.magnitude > 0.5;
+    }
+
+    public bool IsJumping()
+    {
+        return isJumping;
+    }
+}
