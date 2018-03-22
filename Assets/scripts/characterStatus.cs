b24884a06988d393eacf4a267d0648f6dd9f6cba
Unknown <stefan@bigyugo.com>
2018-03-22 14:05:25 -0400

    consolodated the controller script
    need to add sprinting

--- Assets/scripts/characterStatus.cs
+++ Assets/scripts/characterStatus.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class characterStatus : MonoBehaviour {
-    public float health = 10;
+    public float health = 10f;
     public int healthMax = 10;
-    public int stamina = 10;
+    public float stamina = 10f;
     public float heavyAttackValue = 4f;
     public float runValue = 2f;
 
     public Rigidbody2D rb;
-    public playerController pc;
+    public mainController pc;
+
+    public void StaminaDrain(){
+        float drain = .2f;
+
+        stamina -= (drain * Time.deltaTime);
+    }
 
     public float addHealth(float boost) {
         health += boost;
@@ -20,6 +26,11 @@ public class characterStatus : MonoBehaviour {
             health = healthMax;
         }
         return (health);
+    }
+
+    public void Die() {
+        print("dead");
+
     }
 
     //public 
---
+++ Assets/scripts/mainController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainController : MonoBehaviour {
+    float walkSpeed = 10;                       //walk speed
+    public float jumpSpeed = 8;                 //how fast character jump
+    public float runSpeed;                      //run speed
+    public Rigidbody2D rb;                      //initialized the rigbody of the character 
+    public Vector2 vectH = new Vector2(1, 0);   //direction for horizontal movement
+    public Vector2 vectV = new Vector2(0, 1);   //direction of vertical movemnet
+    public bool isJumping = false;              //check for jumping or not
+    float horizontal;                           //input for walking
+    float vertical;                             //input for jumping
+    float jumpH;                                //horizontal movement for jump
+
+	// Use this for initialization
+	void Start () {    
+        jumpH = walkSpeed / 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if (horizontal > 0) {
+            transform.Translate(vectH * walkSpeed * Time.deltaTime);
+        }
+        else if (horizontal < 0){
+            transform.Translate(vectH * -1 * walkSpeed * Time.deltaTime);
+        }
+
+        //jump cojntroll
+        if (rb.velocity.y == 0){
+            if (vertical > 0) {
+                rb.AddForce(vectV * jumpSpeed, ForceMode2D.Impulse);
+                rb.AddForce(vectH * jumpH * Mathf.Round(horizontal), ForceMode2D.Impulse);
+                isJumping = true;
+            }
+            else isJumping = false;
+        }
+
+	}
+}
