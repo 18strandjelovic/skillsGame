d777ffc4860e8da1668a1e98a0f5dc2bbd0d0d9d
Unknown <stefan@bigyugo.com>
2018-04-11 15:00:44 -0400

    seige machine script and other things i forgot

---
+++ Assets/scripts/seigeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seigeController : MonoBehaviour {
+    public float endPos = 30f;
+    public float speed = 8f;
+    public Vector3 direction = new Vector3(1, 0, 0);
+    public float currentPos;
+    public GameObject player;
+    public Collider2D playerCollider;
+
+	// Use this for initialization
+	void Start () {
+        currentPos = transform.position.x;
+        player = GameObject.Find("Player");
+        //Physics2D.IgnoreCollision
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentPos = transform.position.x;
+
+        print("test");
+        if (currentPos < endPos)
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+            currentPos = transform.position.x;
+            
+        }
+	}
+}
